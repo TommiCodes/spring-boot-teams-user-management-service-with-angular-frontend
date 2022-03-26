c17d392d3ee2dbca12fb87a3a2aaf93c3d139c1d
Thomas Kilb <thomaskilb@outlook.com>
2022-03-26 16:53:52 +0100

    reformat auth service

--- angular-teams-users-management-frontend/src/app/public/services/auth-service/auth.service.ts
+++ angular-teams-users-management-frontend/src/app/public/services/auth-service/auth.service.ts
@@ -5,13 +5,13 @@ import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { User } from 'src/app/model/interfaces';
 import {
   AuthenticationResponse,
   LoginRequest,
   RegisterRequest,
 } from '../../model/interfaces';
 import { snackBarConf } from 'src/app/model/consts';
+import { User } from 'src/app/model/user.interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -22,32 +22,20 @@ export class AuthService {
     private snackbar: MatSnackBar,
     private jwtService: JwtHelperService,
     private userState: UserState
-  ) {}
+  ) { }
 
   login(loginRequest: LoginRequest): Observable<AuthenticationResponse> {
-    return this.http
-      .post<AuthenticationResponse>('/api/auth/login', loginRequest)
-      .pipe(
-        tap((res: AuthenticationResponse) =>
-          localStorage.setItem(LOCALSTORAGE_TOKEN_KEY, res.accessToken)
-        ),
-        tap((res: AuthenticationResponse) =>
-          this.userState.setState(this.jwtService.decodeToken(res.accessToken))
-        ),
-        tap(() =>
-          this.snackbar.open('Login Successfull', 'Close', snackBarConf)
-        )
-      );
+    return this.http.post<AuthenticationResponse>('/api/auth/login', loginRequest).pipe(
+      tap((res: AuthenticationResponse) => localStorage.setItem(LOCALSTORAGE_TOKEN_KEY, res.accessToken)),
+      tap((res: AuthenticationResponse) => this.userState.setState(this.jwtService.decodeToken(res.accessToken))),
+      tap(() => this.snackbar.open('Login Successfull', 'Close', snackBarConf))
+    );
   }
 
   register(registerRequest: RegisterRequest): Observable<User> {
-    return this.http
-      .post<User>('/api/users', registerRequest)
-      .pipe(
-        tap(() =>
-          this.snackbar.open(`User created successfully`, 'Close', snackBarConf)
-        )
-      );
+    return this.http.post<User>('/api/users', registerRequest).pipe(
+      tap(() => this.snackbar.open(`User created successfully`, 'Close', snackBarConf))
+    );
   }
 
   getLoggedInUser() {
